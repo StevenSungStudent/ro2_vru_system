7731c7d0375b50c691eee48b67dd2273402df2fb
StevenSungStudent <S.Sung@student.han.nl>
2024-12-18 19:13:07 +0100

    more WIP, not functional

--- src/vru_robot_controller/low_level_controller/include/low_level_controller/mavros_offboard_controller.hpp
+++ src/vru_robot_controller/low_level_controller/include/low_level_controller/mavros_offboard_controller.hpp
@@ -1,30 +1,55 @@
 #ifndef MAVROS_OFFBOARD_CONTROLLER_HPP
 #define MAVROS_OFFBOARD_CONTROLLER_HPP
 
+// NOTE:
+// I used the following example as a guide to write this code:
+// https://docs.px4.io/main/en/ros/mavros_offboard_cpp.html For those unaware:
+// BTW: this is the header file, the source file (where the implementation of the header code is) is in the src folder.
 
+//TODO:
+// is the commandbool bugged? need to check.
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "mavros_msgs/msg/state.hpp"
+#include "mavros_msgs/srv/command_bool.hpp"
+#include "mavros_msgs/srv/set_mode.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "mavros_msgs/msg/state.hpp"
-#include "mavros_msgs/srv/command_bool.hpp"
-#include "mavros_msgs/srv/set_mode.hpp"
 
-class mavros_offboard_controller : public rclcpp::Node {
+class mavros_offboard_controller : public rclcpp::Node
+{
 public:
   mavros_offboard_controller();
   ~mavros_offboard_controller();
 
 private:
-    mavros_msgs::msg::State current_state;
-    rclcpp::Subscription<mavros_msgs::msg::State>::SharedPtr state_subscriber;//current state of the robot. hold, manual, offboard mode, etc.
-    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr local_position_publisher; //for offboard mode happy.
-    rclcpp::Service<mavros_msgs::srv::CommandBool>::SharedPtr arming_client;//arming the robot.
+  // Variables
+  const unsigned short publishing_frequency;
+  mavros_msgs::msg::State current_state;//TODO: maybe redundant.
+  nav_msgs::msg::Odometry current_position;
 
-    rclcpp::Rate publishing_frequency;
 
+  // Subscribers, Publishers and Clients.
+  rclcpp::Subscription<mavros_msgs::msg::State>::SharedPtr state_subscriber;  			      // current state of the robot. hold,// manual, offboard mode, etc.
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscriber;           // location of the robot (or at least where the robot thinks it is).
+
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr local_position_publisher; // for offboard mode happy.
+
+  rclcpp::Client<mavros_msgs::srv::CommandBool>::SharedPtr arming_client;				          // arming the robot.
+  rclcpp::Client<mavros_msgs::srv::SetMode>::SharedPtr mode_setting_client;     
+  rclcpp::TimerBase::SharedPtr timer;                                                     // Timer for the local position publisher.
+
+
+  // Callbacks
+  void state_callback(const mavros_msgs::msg::State& msg);
+  void odometry_callback(const nav_msgs::msg::Odometry& msg);
+  void local_position_callback();
+
+  // Functions
 };
 
-#endif // MAVROS_OFFBOARD_CONTROLLER_HPP
+#endif	// MAVROS_OFFBOARD_CONTROLLER_HPP
--- src/vru_robot_controller/low_level_controller/src/mavros_offboard_controller.cpp
+++ src/vru_robot_controller/low_level_controller/src/mavros_offboard_controller.cpp
@@ -1,12 +1,62 @@
 #include "mavros_offboard_controller.hpp"
+#include <cstdlib>
+#include <iostream>
+using std::placeholders::_1;
 
-mavros_offboard_controller::mavros_offboard_controller() : Node("mavros_offboard_controller")
-{
-  RCLCPP_INFO(rclcpp::get_logger("mavros_offboard_controller"),
-              "Controller initialized");
+//============================================================================================
+//CONSTRUCTOR AND DESTRUCTOR
+mavros_offboard_controller::mavros_offboard_controller() : Node("mavros_offboard_controller"), publishing_frequency(20) {
+  RCLCPP_INFO(rclcpp::get_logger("mavros_offboard_controller"), "Controller initialized");
+
+  // Publishers, subscribers and clients.
+  state_subscriber = this->create_subscription<mavros_msgs::msg::State>( "mavros/state", 10, std::bind(&mavros_offboard_controller::state_callback, this, _1));			
+  odometry_subscriber = this->create_subscription<nav_msgs::msg::Odometry>( "mavros/local_position/odom", 10, std::bind(&mavros_offboard_controller::odometry_callback, this, _1));
+
+  local_position_publisher = this->create_publisher<geometry_msgs::msg::PoseStamped>( "mavros/setpoint_position/local", 10);
+
+  arming_client = this->create_client<mavros_msgs::srv::CommandBool>("mavros/cmd/arming");	
+  mode_setting_client = this->create_client<mavros_msgs::srv::SetMode>("mavros/set_mode");
+  timer = this->create_wall_timer(std::chrono::milliseconds(publishing_frequency),std::bind(&mavros_offboard_controller::local_position_callback, this));
 }
 
 mavros_offboard_controller::~mavros_offboard_controller() {
-  RCLCPP_INFO(rclcpp::get_logger("mavros_offboard_controller"),
-              "Controller destroyed");
+  RCLCPP_INFO(rclcpp::get_logger("mavros_offboard_controller"), "Controller destroyed");
+}
+
+
+//============================================================================================
+//CALLBACKS
+void mavros_offboard_controller::state_callback(const mavros_msgs::msg::State& msg)
+{
+  current_state = msg;
+
+  if(!msg.armed)
+  {
+    std::string command = "ros2 service call /mavros/cmd/arming mavros_msgs/srv/CommandBool \"{value: True}\"";//TODO: this is scuffed, check if works.
+    system(command.c_str());
+  }
+
+  if(msg.mode != "OFFBOARD")
+  {
+    std::string command = "ros2 service call /mavros/set_mode mavros_msgs/srv/SetMode \"{custom_mode: \"OFFBOARD\"}\"";//TODO: this is scuffed, check if works.
+    system(command.c_str());
+  }
+
+}
+
+void mavros_offboard_controller::odometry_callback(const nav_msgs::msg::Odometry& msg)
+{
+  current_position = msg;
+}
+
+void mavros_offboard_controller::local_position_callback()//TODO: This is 100% not how this is supposed to work, need to update later.
+{
+  geometry_msgs::msg::PoseStamped position_message;
+  current_position.pose.pose.position;
+
+  position_message.header.stamp = this->get_clock()->now();
+
+  position_message.pose = current_position.pose.pose;
+
+  local_position_publisher->publish(position_message);
 }
